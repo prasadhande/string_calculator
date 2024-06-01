0985085bff306638f89f67e3bc73aa21ea2b5f60
User <prasadhande@gmail.com>
2024-06-01 11:15:43 +0530

    Signed-off-by: User <prasadhande@gmail.com>

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -24,6 +24,18 @@ require_relative '../string_calculator'
         expect(StringCalculator.add("1,2,3,4")).to eq(10)
       end
     end
+
+    context "when input has new lines between numbers" do
+      it "returns the sum of all numbers" do
+        expect(StringCalculator.add("1\n2,3")).to eq(6)
+      end
+    end
+
+    context "when input has different delimiters" do
+      it "returns the sum of all numbers" do
+        expect(StringCalculator.add("//;\n1;2")).to eq(3)
+      end
+    end
     
   end
 end
