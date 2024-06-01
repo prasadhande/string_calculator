eb86adfd1188d5ee42fb2901c058b0434097463e
User <prasadhande@gmail.com>
2024-06-01 11:16:21 +0530

    Signed-off-by: User <prasadhande@gmail.com>

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -36,6 +36,20 @@ require_relative '../string_calculator'
         expect(StringCalculator.add("//;\n1;2")).to eq(3)
       end
     end
+
+    context "when input has negative numbers" do
+      it "raises an exception with all negative numbers" do
+        expect { StringCalculator.add("1,-2,-3") }.to raise_error("negatives not allowed: -2, -3")
+      end
+    end
+
+    context "when input has numbers bigger than 1000" do
+      it "ignores numbers bigger than 1000" do
+        expect(StringCalculator.add("2,1001")).to eq(2)
+      end
+    end
+
     
+
   end
 end
